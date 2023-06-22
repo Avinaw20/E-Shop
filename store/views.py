71186a9ce53a7a1a5ddb51fb18a37eb49b9dc268
avinaw20 <avinaw20@gmail.com>
2023-06-22 12:39:32 +0530

    Form Validations in Process

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
 from .models import Category
+from .models import Customer
 # Create your views here.
 def index(request):
     products = None
@@ -23,4 +24,29 @@ def index(request):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        #validation
+        error_message = None
+
+        if(not first_name):
+            error_message = "First Name Required !!"
+        elif len(first_name) < 3:
+                error_message = 'First Name should be more than 3 characters'
+            
+
+        #saving
+        if not error_message:
+            customer = Customer(first_name=first_name, last_name=last_name, phone=phone, email=email, password=password)
+            customer.register()
+        else:
+            return HttpResponse(request,'signup.html', {'error': error_message})
+
+
+
